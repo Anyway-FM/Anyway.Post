baa5cb5130c1073a12cb22023d52a4c28ddf66ea
JJ Ying <yingjunjiu@gmail.com>
2017-02-24 11:33:01 +0800

    Updated Gulp Settings.

--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,27 @@ var gulp = require('gulp'),
     gulpLoadPlugins = require('gulp-load-plugins'),
     plugins = gulpLoadPlugins();
  
+var cdnUrl = [
+	[ 'build/', 'http://anyway-web.b0.upaiyun.com/anyway.post/' ]
+];
+ 
 gulp.task('default', function() {
 	gulp.watch(['*','*/*'], ['build']);
  });
  
-gulp.task('build', ['watch'], function() {
-  return gulp.src(['css/*.css','!css/*.min.css'])
-    .pipe(plugins.cleanCss({compatibility: 'ie8'}))
-    .pipe(plugins.rename({
-          suffix: '.min'
-        }))
-    .pipe(gulp.dest('css'));
-    
-   return gulp.src(['assets/*.svg','!assets/*.min.svg'])
-           .pipe(plugins.svgmin())
-           .pipe(plugins.rename({
-                 suffix: '.min'
-               }))
-           .pipe(gulp.dest('assets'));
+ 
+gulp.task('build', function() {
+
+	gulp.src('*.html')
+		.pipe(plugins.batchReplace(cdnUrl))
+		.pipe(plugins.htmlmin({collapseWhitespace: true}))
+		.pipe(gulp.dest('build'));
+		
+	gulp.src(['css/*.css','!css/*.min.css'])
+		.pipe(plugins.cleanCss({compatibility: 'ie8'}))
+		.pipe(plugins.rename({
+		      suffix: '.min'
+		    }))
+		.pipe(gulp.dest('build'));     
 });
 
-gulp.task('watch', function () {
-   gulp.start('build');
-});
