8e06d20abc84cb8169de945c62904cdb6cb0d469
JJ Ying <yingjunjiu@gmail.com>
2017-08-21 17:25:00 +0800

    增加了邮报的 Markdown 向邮件 HTML 转换功能

--- gulpfile.js
+++ gulpfile.js
@@ -34,6 +34,32 @@ gulp.task('default', function() {
 		.pipe(gulp.dest('builds'));     
 });
 
+gulp.task('md', function() {
+	var mds = require('markdown-styles'),
+	    path = require('path');
+	
+	mds.render(mds.resolveArgs({
+	  input: path.normalize(process.cwd() + '/Posts/Markdown'),
+	  output: path.normalize(process.cwd() + '/Posts/HTML'),
+	  layout: path.normalize(process.cwd() + '/Posts/Theme'),
+	}), function() {
+	  console.log('All done!');
+	});
+	
+	var htmlTheme = [
+		[ '<a href', '<a style=\"color:#f60c3e !important;\" href' ],
+		[ '<p', '<p style=\"color:#555 !important;font-size:14px;\"' ],
+		[ '<li', '<li style=\"color:#555 !important;font-size:14px;\"' ],
+		[ '<h1 ', '<h1 style=\"color:#333;font-weight:700 !important;font-size:1.5em;margin-top:2em;\" ' ]
+	];
+	
+	gulp.src('Posts/HTML/*.html')
+		.pipe(plugins.batchReplace(htmlTheme))
+		.pipe(gulp.dest('posts/HTML/'));
+	
+ });
+
+
 gulp.task('watch', function() {
 	gulp.watch(['*','*/*'], ['default']);
  });
